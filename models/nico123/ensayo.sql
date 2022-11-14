18aa5ff0d7d12e25f06821f2f30e2cf4622d3c96
nicolasgarciapinto00@gmail.com <nicolasgarciapinto00@gmail.com>
2022-11-14 06:03:16 +0000

    Primer modelo prueba

--- models/nico123/ensayo.sql
+++ models/nico123/ensayo.sql
@@ -2,4 +2,4 @@ Select
 id,
 usuarios,
 (cast(substring(MILLONESDESEGUIDORES,1,charindex('k',MILLONESDESEGUIDORES)-1) as number(15,2)))*1000000000 as Seguidores
-from clasifseguid;
+from clasifseguid
